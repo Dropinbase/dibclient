db09e630922f4148636504fec8ba0c17cd259e03
dawie-git <dawieblake@gmail.com>
2017-08-17 22:28:41 +0200

    file references fixed

--- files.php
+++ files.php
@@ -1,4 +1,4 @@
 <?php
 $DIR = __DIR__;
-include_once("../dropinbase/files.php");
-//include_once("./vendor/dropinbase/dropinbase/files.php"); // rather adjust .htaccess?
+//include_once("../dropinbase/files.php");
+include_once("./vendor/dropinbase/dropinbase/files.php"); // rather adjust .htaccess?
