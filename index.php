72edbfb78995070405a9091cedc9790c5e7b5916
dawie-git <dawieblake@gmail.com>
2017-08-17 15:07:18 +0200

    composer

--- index.php
+++ index.php
@@ -2,5 +2,4 @@
     //phpinfo(); // To view installed PHP extensions, uncomment this line, and comment out all other lines below
     
     $DIR = __DIR__;
-    include_once("../dropinbase/index.php");
-    //include_once("./vendor/dropinbase/dropinbase/index.php");
+    include_once("./vendor/dropinbase/dropinbase/index.php");
