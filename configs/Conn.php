8a2b8025d740f6cf257b8160074e1f335592e976
dawie-git <dawieblake@gmail.com>
2017-08-25 06:42:36 +0200

    Update Conn.php

--- configs/Conn.php
+++ configs/Conn.php
@@ -3,9 +3,9 @@
 // NOTES: The index of the main dropinbase table must match the DBINDEX value in Dib.php  
 //        Using an IP address as host (eg 127.0.0.1 instead of 'localhost') can dramatically increase performance
 
-DIB::$DATABASES = array( // dropinbase, glasschem_dib
+DIB::$DATABASES = array(
 
-   1 => array('database'=>'dropinba_ngdev', 'dbType'=>'mysql', 'connectionString'=>'mysql:dbname=dropinba_ngdev;host=127.0.0.1;charset=utf8', 'charset'=>'utf8',
+   1 => array('database'=>'dropinbase', 'dbType'=>'mysql', 'connectionString'=>'mysql:dbname=dropinba_ngdev;host=127.0.0.1;charset=utf8', 'charset'=>'utf8',
               'username'=>'root', 'password'=>'123456', 'host'=>'127.0.0.1', 'port'=>'', 'emulatePrepare'=>true,  'dbDropin'=>'dibMySqlPdo', 'systemDropin'=>true),      
 
      
