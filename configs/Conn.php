464700e6632db1c058fe3d3fb694050ed9871f67
dawie-git <dawieblake@gmail.com>
2017-08-25 06:36:58 +0200

    Update Conn.php

--- configs/Conn.php
+++ configs/Conn.php
@@ -4,19 +4,11 @@
 //        Using an IP address as host (eg 127.0.0.1 instead of 'localhost') can dramatically increase performance
 
 DIB::$DATABASES = array( // dropinbase, glasschem_dib
-	//1 => array('database'=>'dropinba_ngdev', 'dbType'=>'mysql', 'connectionString'=>'mysql:dbname=dropinba_ngdev;host=dropinbase.com;charset=utf8', 'username'=>'dropinba_ngdev',
-   //           'password'=>'h1bKPiDgrqV1', 'host'=>'dropinbase.com', 'port'=>'', 'emulatePrepare'=>true, 'charset'=>'utf8', 'dbDropin'=>'dibMySqlPdo', 'systemDropin'=>true),      
 
    1 => array('database'=>'dropinba_ngdev', 'dbType'=>'mysql', 'connectionString'=>'mysql:dbname=dropinba_ngdev;host=127.0.0.1;charset=utf8', 'charset'=>'utf8',
               'username'=>'root', 'password'=>'123456', 'host'=>'127.0.0.1', 'port'=>'', 'emulatePrepare'=>true,  'dbDropin'=>'dibMySqlPdo', 'systemDropin'=>true),      
 
-   7 => array('database'=>'dib_demo', 'dbType'=>'mysql', 'connectionString'=>'mysql:dbname=dib_demo;host=127.0.0.1;charset=utf8', 'charset'=>'utf8',
-              'username'=>'root', 'password'=>'123456', 'host'=>'127.0.0.1', 'port'=>'', 'emulatePrepare'=>true,  'dbDropin'=>'dibMySqlPdo', 'systemDropin'=>true),      
- 
-   8 => array('database'=>'apros', 'dbType'=>'mysql', 'connectionString'=>'mysql:dbname=apros;host=127.0.0.1;charset=utf8', 'username'=>'root',
-              'password'=>'123456', 'host'=>'127.0.0.1', 'port'=>'', 'emulatePrepare'=>true, 'charset'=>'utf8', 'dbDropin'=>'dibMySqlPdo', 'systemDropin'=>true), 
-   
-   
+     
 
 ); 
 
