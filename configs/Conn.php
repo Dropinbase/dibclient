77cd62e9ab236149bcb0476940f9eada02d2e0eb
dawie-git <dawieblake@gmail.com>
2017-10-04 17:11:01 +0200

    improved layout

--- configs/Conn.php
+++ configs/Conn.php
@@ -1,15 +1,20 @@
-<?php 
+<?php
 
-// NOTES: The index of the main dropinbase table must match the DBINDEX value in Dib.php  
+// NOTES: The index of the main dropinbase table must match the DBINDEX value in Dib.php
 //        Using an IP address as host (eg 127.0.0.1 instead of 'localhost') can dramatically increase performance
 
 DIB::$DATABASES = array(
-
-   1 => array('database'=>'dropinbase', 'dbType'=>'mysql', 'connectionString'=>'mysql:dbname=dropinbase;host=127.0.0.1;charset=utf8', 'charset'=>'utf8',
-              'username'=>'root', 'password'=>'123456', 'host'=>'127.0.0.1', 'port'=>'', 'emulatePrepare'=>true,  'dbDropin'=>'dibMySqlPdo', 'systemDropin'=>true),      
-  // 3 => array('database'=>'dib_demo', 'dbType'=>'mysql', 'connectionString'=>'mysql:dbname=dib_demo;host=127.0.0.1;charset=utf8', 'charset'=>'utf8',
-  //            'username'=>'root', 'password'=>'123456', 'host'=>'127.0.0.1', 'port'=>'', 'emulatePrepare'=>true,  'dbDropin'=>'dibMySqlPdo', 'systemDropin'=>true), 
-     
-
+1 => array(
+        'database' => 'dropinbase',
+        'dbType' => 'mysql',
+        'connectionString' => 'mysql:dbname=dropinbase;host=127.0.0.1;charset=utf8',
+        'charset' => 'utf8',
+        'username' => 'root',
+        'password' => '123456',
+        'host' => '127.0.0.1',
+        'port' => null,               
+        'emulatePrepare' => true,
+        'dbDropin' => 'dibMySqlPdo',
+        'systemDropin' => true
+    ),
 );
-
