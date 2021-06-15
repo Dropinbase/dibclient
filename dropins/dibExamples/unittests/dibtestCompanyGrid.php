f46d776bd59053f5343dbee88f1b07c26632adf1
dawie-git <dawieblake@gmail.com>
2021-06-15 14:40:30 +0200

    unit testing

--- dropins/dibExamples/unittests/dibtestCompanyGrid.php
+++ dropins/dibExamples/unittests/dibtestCompanyGrid.php
@@ -1,17 +1,18 @@
 <?php 
 
-// dibtestCompanyGrid
+$params = array('key'=> 'unitTesting', 'value' => true);
+ClientFunctions::addAction($actionList, 'set-environment-value', $params);
 
+// dibtestCompanyGrid
+/*
 $params = array('action'=>'xlsx', 'actionType'=>'itemAlias',  'trigger'=>'click', 'containerName'=>$containerName, 'portId'=>null); // item where container is injected
 ClientFunctions::addAction($actionList, 'run', $params);
 
 $params = array('action'=>'xlsxA', 'actionType'=>'itemAlias',  'trigger'=>'click', 'containerName'=>$containerName, 'portId'=>null);
 ClientFunctions::addAction($actionList, 'run', $params);
+*/
 
 
-
-$params = array('action'=>'btnAdd', 'actionType'=>'itemAlias', 'trigger'=>'click', 'containerName'=>$containerName, 'portId'=>null);
-ClientFunctions::addAction($actionList, 'run', $params);
 
 /*
 $params = array('action'=>'addRecord', 'containerName'=>$containerName, 'portId'=>null);
@@ -29,6 +30,10 @@ $containerName = 'dibtestCompanyForm';
 // TODO -> new actions: set-model-value   AND  set-list-value
 $params = array('name'=>substr(uniqid(), 9, 5), 'chinese_name'=>substr(uniqid(), 9, 5), 'parent_company_id'=>1);
 ClientFunctions::addAction($actionList, 'set-value', $params);
+
+$params = array('action'=>'btnPrompt', 'actionType'=>'itemAlias', 'trigger'=>'click', 'containerName'=>$containerName, 'portId'=>null);
+ClientFunctions::addAction($actionList, 'run', $params);
+
 
 $params = array('action'=>'btnsave', 'actionType'=>'itemAlias', 'trigger'=>'click', 'containerName'=>$containerName, 'portId'=>null);
 ClientFunctions::addAction($actionList, 'run', $params);
