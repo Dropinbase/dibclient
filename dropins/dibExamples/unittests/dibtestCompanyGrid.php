f1703495e34de851d93049e2a14022d220bf49b1
cobus_coetsee <cobus@matogen.com>
2021-06-15 14:41:20 +0200

    UPdate unit tests

--- dropins/dibExamples/unittests/dibtestCompanyGrid.php
+++ dropins/dibExamples/unittests/dibtestCompanyGrid.php
@@ -4,14 +4,12 @@
 /*
 $params = array('action'=>'xlsx', 'actionType'=>'itemAlias',  'trigger'=>'click', 'containerName'=>$containerName, 'portId'=>null); // item where container is injected
 ClientFunctions::addAction($actionList, 'run', $params);
-
 $params = array('action'=>'xlsxA', 'actionType'=>'itemAlias',  'trigger'=>'click', 'containerName'=>$containerName, 'portId'=>null);
 ClientFunctions::addAction($actionList, 'run', $params);
 */
 
-
-$params = array('action'=>'btnAdd', 'actionType'=>'itemAlias', 'trigger'=>'click', 'containerName'=>$containerName, 'portId'=>null);
-ClientFunctions::addAction($actionList, 'run', $params);
+// $params = array('action'=>'btnAdd', 'actionType'=>'itemAlias', 'trigger'=>'click', 'containerName'=>$containerName, 'portId'=>null);
+// ClientFunctions::addAction($actionList, 'run', $params);
 
 /*
 $params = array('action'=>'addRecord', 'containerName'=>$containerName, 'portId'=>null);
@@ -22,19 +20,19 @@ ClientFunctions::addAction($actionList, 'ContainerActions', $params);
 $params = array('action'=>'btnEdit', 'actionType'=>'itemAlias',  'trigger'=>'click', 'containerName'=>$containerName, 'portId'=>null);
 ClientFunctions::addAction($actionList, 'run', $params);
 */
+$params = array('key'=> 'unitTesting', 'value' => true);
+ClientFunctions::addAction($actionList, 'set-environment-value', $params);
 
 // dibtestCompanyForm
 $containerName = 'dibtestCompanyForm';
 
-// TODO -> new actions: set-model-value   AND  set-list-value
-$params = array('name'=>substr(uniqid(), 9, 5), 'chinese_name'=>substr(uniqid(), 9, 5), 'parent_company_id'=>1);
-ClientFunctions::addAction($actionList, 'set-value', $params);
+// TODO -> new actions: set-model-value AND set-list-value
+$params = array('parent_company_id'=> 'Global');
+ClientFunctions::addAction($actionList, 'set-value-list', $params);
+//sleep(5);
+// $params = array('action'=>'btnsave', 'actionType'=>'itemAlias', 'trigger'=>'click', 'containerName'=>$containerName, 'portId'=>null);
+// ClientFunctions::addAction($actionList, 'run', $params);
 
-$params = array('action'=>'btnsave', 'actionType'=>'itemAlias', 'trigger'=>'click', 'containerName'=>$containerName, 'portId'=>null);
-ClientFunctions::addAction($actionList, 'run', $params);
-
-
-
-
-
+$params = array('defaultValue'=> 'Hello', 'close' => true);
+ClientFunctions::addAction($actionList, 'set-prompt-value', $params);
 
